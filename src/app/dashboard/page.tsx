9efceddb4754abb4c7a75bc094c64ecc657c7736
Pawel Niewiadowski <speakpolishpl@gmail.com>
2024-03-16 14:22:10 +0100

    add aside filter

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,30 +3,43 @@
 import "dayjs/locale/pl";
 import dayjs from "dayjs";
 import styles from "./page.module.css";
+import Avatar from "@mui/material/Avatar";
+import SettingsIcon from "@mui/icons-material/Settings";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import AsideFilter from "@/components/AsideFilter";
 export default function Dashboard() {
   return (
     <main className={styles.main}>
-      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pl">
-        <div>
-          <DatePicker
-            defaultValue={dayjs().subtract(1, "week")}
-            disableFuture
-            slotProps={{
-              field: { className: styles.date_picker_left },
-            }}
-          />
-          <DatePicker
-            defaultValue={dayjs()}
-            disableFuture
-            slotProps={{
-              field: { className: styles.date_picker_right },
-            }}
-          />
-        </div>
-      </LocalizationProvider>
+      <header className={styles.header}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pl">
+          <div>
+            <Avatar sx={{ width: 56, height: 56 }} /> Mr Muffin
+          </div>
+          <div>
+            <DatePicker
+              defaultValue={dayjs().subtract(1, "week")}
+              disableFuture
+              slotProps={{
+                field: { className: styles.date_picker_left },
+              }}
+            />
+            <DatePicker
+              defaultValue={dayjs()}
+              disableFuture
+              slotProps={{
+                field: { className: styles.date_picker_right },
+              }}
+            />
+          </div>
+          <div>
+            <SettingsIcon fontSize="large" />
+          </div>
+        </LocalizationProvider>
+      </header>
+      <article></article>
+      <AsideFilter />
     </main>
   );
 }
---
+++ src/components/AsideFilter.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./css/asideFilter.module.css";
+import TodayIcon from "@mui/icons-material/Today";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import AllInclusiveIcon from "@mui/icons-material/AllInclusive";
+import EventRepeatIcon from "@mui/icons-material/EventRepeat";
+
+function AsideFilter() {
+  return (
+    <aside className={styles.aside}>
+      <div>
+        <TodayIcon className={styles.asideIcon} />
+      </div>
+      <div>
+        <DateRangeIcon className={styles.asideIcon} />
+      </div>
+      <div>
+        <CalendarMonthIcon className={styles.asideIcon} />
+      </div>
+      <div>
+        <EventRepeatIcon className={styles.asideIcon} />
+      </div>
+      <div>
+        <AllInclusiveIcon className={styles.asideIcon} />
+      </div>
+    </aside>
+  );
+}
+
+export default AsideFilter;
