192e9b634c19f28c7fbe2a28a8fa560903378b77
Pawel Niewiadowski <speakpolishpl@gmail.com>
2024-04-01 19:27:06 +0200

    add other inputs

--- src/app/dashboard/add/page.tsx
+++ src/app/dashboard/add/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import "dayjs/locale/pl";
 import styles from "./page.module.scss";
 import InputAdornment from "@mui/material/InputAdornment";
-import Input from "@mui/material/Input";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs from "dayjs";
+import { useRouter } from "next/navigation";
+import Button from "@mui/material/Button";
 
 export default function AddExpence({}) {
   const [amount, setAmount] = useState(0);
   const [amountVisible, setAmountVisible] = useState("");
   const [amountHelperText, setAmountHelperText] = useState("");
+  const [comment, setComment] = useState("");
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
 
   function isCommaFirst(a: string) {
     if (!a.match(/,/)) return false;
@@ -56,36 +66,76 @@ export default function AddExpence({}) {
       setAmountVisible("");
     }
   }
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    setIsLoading(true);
+    event.preventDefault();
+
+    const response = await fetch("/api/addItem", {
+      method: "POST",
+    });
+    console.log(response);
+    if (response.ok) {
+      router.push("/dashboard");
+    } else {
+      console.error("add error");
+    }
+
+    setIsLoading(false);
+  }
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <div>
           <p>Add expence</p>
         </div>
-        <div>
-          <p>___ Currency</p>
-          <TextField
-            id="standard-adornment-amount"
-            variant="standard"
-            InputProps={{
-              endAdornment: <InputAdornment position="end">$</InputAdornment>,
-            }}
-            autoFocus
-            error={amountHelperText ? true : false}
-            helperText={amountHelperText}
-            value={amountVisible}
-            onChange={(e) => validateAmount(e.target.value)}
-          />
-        </div>
-        <div>
-          <p>Category</p>
-        </div>
-        <div>
-          <p>Date</p>
-        </div>
-        <div>
-          <p>Comment</p>
-        </div>
+        <form onSubmit={onSubmit}>
+          <div>
+            <TextField
+              variant="standard"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">$</InputAdornment>,
+              }}
+              autoFocus
+              error={amountHelperText ? true : false}
+              helperText={amountHelperText}
+              value={amountVisible}
+              onChange={(e) => validateAmount(e.target.value)}
+            />
+          </div>
+          <div>
+            <p>Category</p>
+          </div>
+          <div>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pl">
+              <DatePicker
+                defaultValue={dayjs()}
+                slotProps={{
+                  field: { className: styles.date_picker },
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+          <div>
+            <TextField
+              variant="outlined"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              multiline
+              fullWidth
+              label="Comment"
+            />
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              type="submit"
+              size="large"
+              disabled={isLoading}
+            >
+              {isLoading ? "Loading..." : "Add"}
+            </Button>
+          </div>
+        </form>
       </div>
     </main>
   );
