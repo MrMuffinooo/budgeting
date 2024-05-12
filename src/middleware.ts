b8c8e65fe048948900d345886ec0b6194dc97615
Pawel Niewiadowski <speakpolishpl@gmail.com>
2024-05-12 22:49:26 +0200

    handle invalid access token

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,15 @@ import {
 } from "@aws-sdk/client-cognito-identity-provider";
 
 export async function middleware(request: NextRequest) {
+  if (
+    request.nextUrl.pathname.startsWith("/_next") || // exclude Next.js internals
+    request.nextUrl.pathname.startsWith("/api") || //  exclude all API routes
+    request.nextUrl.pathname.startsWith("/static") || // exclude static files
+    /\.(.*)$/.test(request.nextUrl.pathname) // exclude all files in the public folder
+  ) {
+    return NextResponse.next();
+  }
+  console.log(request.nextUrl.pathname);
   const cognitoClient = new CognitoIdentityProviderClient({
     region: process.env.AWS_COGNITO_REGION,
   });
@@ -25,11 +34,12 @@ export async function middleware(request: NextRequest) {
         }
         return NextResponse.next();
       } else {
-        cookieStore.delete("AccessToken");
         throw new Error("invalid access token");
       }
     } catch (e) {
-      return NextResponse.redirect(new URL("/login", request.url));
+      const res = NextResponse.redirect(new URL("/login", request.url));
+      res.cookies.delete("AccessToken");
+      return res;
     }
   }
   if (request.nextUrl.pathname.startsWith("/login")) {
@@ -37,3 +47,22 @@ export async function middleware(request: NextRequest) {
   }
   return NextResponse.redirect(new URL("/login", request.url));
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
